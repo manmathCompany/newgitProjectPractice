ebd3a85911bbcc6ebcbeaa470f629e2a34f6eca2
TL <TL@gmail.com>
2020-10-04 17:37:06 +0530

    first comit

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+package com.mn.model;
+
+public class CalCulator {
+
+	public int add(int a,int b) {
+		
+		return a+b;
+	}
+	
+	public long multi(int a,int b) {
+		
+		return a*b;
+	}
+public int sub(int a,int b){
+return a-b;
+}
