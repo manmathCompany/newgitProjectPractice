047fafab2fa90f29052fbdcd6ac7cc7ba515b367
TL <TL@gmail.com>
2020-10-05 11:04:43 +0530

    adding div method

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,12 @@ public class CalCulator {
 		
 		return a*b;
 	}
-public int sub(int a,int b){
-return a-b;
-}
+       public int sub(int a,int b){
+           return a-b;
+       }
+      
+      public double div(int a,int b){
+          return a/b;
+      }
+
+  }
