cb2f1051f96185e8a08c9bc5b5096fb06b995812
TL <TL@gmail.com>
2020-10-05 11:32:54 +0530

    modiffied

--- Calculator.java
+++ Calculator.java
@@ -11,12 +11,14 @@ public class CalCulator {
 		
 		return a*b;
 	}
-       public int sub(int a,int b){
-           return a-b;
-       }
-      
-      public double div(int a,int b){
-          return a/b;
-      }
 
-  }
+      
+   public int sub(int a,int b){
+     return a-b;
+    }
+   
+   public double div(int a,int b){
+      return a/b;
+   }
+}
+
